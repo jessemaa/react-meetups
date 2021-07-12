d1b51b99c7c8c1e0e315102a375849571e9a743a
Jesse Saarimaa <43336202+jessemaa@users.noreply.github.com>
2021-07-12 14:56:11 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import { FavoritesContextProvider } from './store/favorites-context'
+
+ReactDOM.render(
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>,
+  document.getElementById('root')
+)
